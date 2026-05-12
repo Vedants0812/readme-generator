0e04c9a7cde93cc85982f902743ce132aa5c2ea4
Vedant Suryawanshi <146538646+Vedants0812@users.noreply.github.com>
2026-05-12 23:37:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -73,3 +73,8 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
+
+catch (err) {
+  console.error("FULL ERROR:", err); // 👈 ADD THIS
+  res.status(500).json({ error: err.message });
+}
